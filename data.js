d5dfad6570d416df834cbcba3ce359643e829893
AuliaSabita <112945421+AuliaSabita@users.noreply.github.com>
2022-12-23 19:05:21 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+const dara = {
+    type: "FeatureCollection",
+    features: [
+        {
+            type: "Feature",
+            geometry: {
+                type: "Point",
+                coordinate: [-6.955095961546329, 106.81538227657718]
+            }
+        },
+    ]
+}
